49f968f95391b75c52c9e2f7ccac4c6bb25b3041
Brad Bernard <bradbernard@me.com>
2017-02-01 15:08:42 -0800

    Making it look more real

--- app/Http/Controllers/Scrape/ScrapeController.php
+++ app/Http/Controllers/Scrape/ScrapeController.php
@@ -14,7 +14,21 @@ class ScrapeController extends Controller
     
     public function __construct()
     {
-        $options = array_merge(['base_uri'  => $this->baseUri], $this->options);
+        $options = array_merge(['base_uri'  => $this->baseUri, 'headers' => $this->headers()], $this->options);
         $this->client = new Client($options);
+    }
+
+    private function headers()
+    {
+        return [
+            'Pragma'            => 'no-cache',
+            'Origin'            => 'www.predictit.org',
+            'Accept-Encoding'   => 'gzip, deflate, br',
+            'User-Agent'        => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.13 Safari/537.36',
+            'Accept'            => '*/*',
+            'Cache-Control'     => 'no-cache',
+            'Connection'        => 'keep-alive',
+            'Dnt'               => 1,
+        ];
     }
 }
