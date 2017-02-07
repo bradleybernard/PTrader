1d2acb767c5086c58f3e6427bbd9a3c7e42899c0
Brad Bernard <bradbernard@me.com>
2017-02-07 11:56:24 -0800

    Switched sign

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $closingSoon = Market::where('active', true)->where('status', true)->where('date_end', '>=', \Carbon\Carbon::now())->count();
+        $closingSoon = Market::where('active', true)->where('status', true)->where('date_end', '<=', \Carbon\Carbon::now())->count();
         if($closingSoon > 0) {
             $schedule->call('App\Http\Controllers\Scrape\MarketController@scrape')
                 ->everyMinute();
