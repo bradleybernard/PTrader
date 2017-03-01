89345988a05b2a7b438de041d81f88b8b7130f15
Brad Bernard <bradbernard@me.com>
2017-03-01 15:53:27 -0800

    Updated to human date on accnts

--- resources/views/include/account.blade.php
+++ resources/views/include/account.blade.php
@@ -41,7 +41,7 @@
                                 </td>
                                 <td class="text-center">${{ $account->gain_loss }}</td>
                                 <td class="text-center">${{ $account->invested }}</td>
-                                <td class="text-center">{{ $account->updated_at }}</td>
+                                <td class="text-center">{{ \Carbon\Carbon::now()->diffForHumans(\Carbon\Carbon::parse($account->updated_at)) }}</td>
                             </tr>
                         </tbody>
                     </table>
