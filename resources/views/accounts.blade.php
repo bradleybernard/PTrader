380529493f0442cb9db22311cb2d30306220038e
Brad Bernard <bradbernard@me.com>
2017-03-01 15:44:02 -0800

    Added markets link

--- resources/views/accounts.blade.php
+++ resources/views/accounts.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Accounts')
 @section('content')
     <div class="container" style="padding-top: 40px; padding-bottom: 20px;">
+        <div class="text-center row" style="margin-bottom: 20px;">
+            <div class="col-lg-12">
+                <a href="/markets">Markets</a>
+            </div>
+        </div>
         @each('include/account', $accounts, 'account')
     </div>
 @endsection
